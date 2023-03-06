f48b7d6715023d2f5363867ac3b4c08727b3f439
GiorgiShushanashvili <shushanashvili20@gmail.com>
2023-03-07 02:48:33 +0400

    new change

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("Enter your firstname");
+var name = Console.ReadLine();
+Console.WriteLine("Enter your lastname");
+var lastname = Console.ReadLine();
+Console.WriteLine($"my name is {name} and my lastname is {lastname}");
+
+var age = 23;
+Console.WriteLine($"{name}'s age is {age} years old");
+Console.WriteLine("I love playing football");
+Console.ReadLine();
+
 
